0900ed4229f7421772381409ee12aa97941b5a3c
hmjfinal <34643203+hmjfinal@users.noreply.github.com>
2019-06-16 21:24:13 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@
 	      event.preventDefault();
 	      console.log("Create entry");
 	  	var name= document.querySelector('#name').value ||  '访客';
-	  	var email= document.querySelector('#email').value ||  'hmj1160927411@gmail';
+	  	var email= document.querySelector('#email').value ||  'hmj1160927441@gmail.com';
 	      const result = await fetch('/create/'+name+'/'+email);
 	      const json = await result.json();
 	      console.log(json.id);
